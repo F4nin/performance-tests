062b800c788de9b8a8ae04e6025a67872a2a308b
F4nin <tovstoles.aleksei@gmail.com>
2026-05-08 11:51:19 +0300

    Добавил docstring для accounts

--- clients/http/gateway/accounts/accounts_schema.py
+++ clients/http/gateway/accounts/accounts_schema.py
@@ -4,6 +4,9 @@ from clients.http.gateway.cards.cards_schema import CardSchema
 
 
 class AccountViewSchema(BaseModel):
+    """
+     Описание структуры аккаунта.
+     """
     id: str
     type: str
     cards: list[CardSchema]
@@ -12,16 +15,31 @@ class AccountViewSchema(BaseModel):
 
 
 class GetAccountsQuerySchema(BaseModel):
+    """
+    Структура данных для получения списка счетов пользователя.
+    """
     user_id: str = Field(alias="userId")
 
 class OpenDepositAccountRequestSchema(BaseModel):
+    """
+    Структура данных для открытия депозитного счета.
+    """
     user_id: str = Field(alias="userId")
 
 class OpenSavingsAccountRequestSchema(BaseModel):
+    """
+    Структура данных для открытия сберегательного счета.
+    """
     user_id: str = Field(alias="userId")
 
 class OpenDebitCardAccountRequestSchema(BaseModel):
+    """
+    Структура данных для открытия дебетового счета.
+    """
     user_id: str = Field(alias="userId")
 
 class OpenCreditCardAccountRequestSchema(BaseModel):
+    """
+    Структура данных для открытия кредитного счета.
+    """
     user_id: str = Field(alias="userId")
